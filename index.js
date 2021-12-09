94299eb3e6f73da8eb55c84aacfc71031dbde561
rrojano <rrojano@gmail.com>
2021-12-09 08:52:14 -0600

    primera propuesta

---
+++ index.js
@@ -0,0 +1,38 @@
+$.ajax({
+    type: "GET",
+    url: "propuesta_desempeño_docente.csv",
+    dataType: "text",
+    success: function (response) {
+        data = $.csv.toArrays(response);
+        generateExam(data);
+    }
+});
+
+function generateExam(data) {
+    var html = '<div class="card">';
+
+    if (typeof (data[0]) === 'undefined') {
+        return null;
+    } else {
+        $.each(data, function (indexP, row) {
+            //borrar header
+            if (indexP == 0) {
+
+            } else {
+                $.each(row, function (indexC, colData) {
+                    console.log(colData)
+                    if (indexC == 0)
+                        html += '<div class="card-header bg-info text-white">' + colData + '</div> <div class="card-body"> '
+                    else {
+                        if (colData != '')
+                            html += '<div class="form-check form-check-inline"> <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault'+indexP+indexC+'">'
+                            html += '<label class="form-check-label" for="flexCheckDefault'+indexP+indexC+'">'+colData + '</label></div>'
+                    }
+                });
+                html += '</div>';
+            }
+        });
+        html += '</div>';
+        $('#csv-display').append(html);
+    }
+}
